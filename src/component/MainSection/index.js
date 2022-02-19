b839d6b814f338066369224032b295613fdfe6d0
rezarekt <theveyst12@gmail.com>
2022-02-20 07:40:37 +0800

    Update
    Minting is now working

--- src/component/MainSection/index.js
+++ src/component/MainSection/index.js
@@ -15,14 +15,65 @@ import { MainContainer,
 import bg from '../../images/bg.png';
 
 const MainSection = ({
+    userAddress,
     connectWallet,
     loading,
-    whitelistMint,
-    publicMint,
-    walletConnected
+    walletConnected,
+    scareBears,
+    publicSalePrice,
+    whitelistSalePrice,
+    setLoading,
+    whitelistSale
 }) => {
+    const {MerkleTree} = require ('merkletreejs');
+    const keccak256 = require ('keccak256');
 
     const [counter, setCounter] = useState(0);
+
+    let whitelistAddresses = ["0x7Da8e9089A94b6D353E3AD13D76B8a765EA2038D", "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0x02e3451AA4Ec12Cb350b3d969a6057ef6ECc96ef"];
+    const leafNode = whitelistAddresses.map(addr => keccak256(addr));
+    const merkleTree = new MerkleTree(leafNode, keccak256, {sortPairs: true});
+
+
+
+    const hexProof = () =>{
+        let index = whitelistAddresses.indexOf(userAddress);
+        return merkleTree.getHexProof(leafNode[index]);
+    }
+
+    const merkleRoot = () =>{
+        return merkleTree.getRoot().toString('hex');
+    }
+
+    const whitelistMint = async() =>{
+        const price = String(parseInt(whitelistSalePrice) * counter);
+        console.log('0x'+merkleRoot());
+        try{
+            await scareBears.methods.whitelistMint(hexProof(), counter).send({value: price, from: userAddress});
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    const publicMint = async() =>{
+        const price = String(parseInt(publicSalePrice) * counter);
+        console.log('0x'+merkleRoot());
+        try{
+            await scareBears.methods.publicMint(hexProof(), counter).send({value: price, from: userAddress});
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    const mintFunction = async() =>{
+        if(whitelistSale){
+            whitelistMint();
+            return;
+        }
+        publicMint();
+    }
 
     const plusButton = () =>{
         setCounter(counter+1);
@@ -34,6 +85,7 @@ const MainSection = ({
         }
         setCounter(counter-1);
     }
+
 
   return (
     <>
@@ -51,7 +103,7 @@ const MainSection = ({
                         <PlusButton onClick={plusButton}>+</PlusButton>
                     </CounterContainer>
                     <CurrentPrice>PRICE: {(counter * 0.069).toFixed(5)} ETH</CurrentPrice>
-                    <ConnectButton onClick={connectWallet}>{walletConnected ? 'Mint' : 'Connect Wallet'}</ConnectButton>
+                    <ConnectButton onClick={walletConnected ? mintFunction : connectWallet}>{walletConnected ? 'Mint' : 'Connect Wallet'}</ConnectButton>
                 </MainBoxContent>
             </MainBoxContainer>
         </MainContainer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,20 @@
 import React, {useState} from 'react'
 import Web3 from 'web3';
-import ScareBearsABI from '../ABI/ScareBears.json';
+import ScareBearsABI from '../ABI/SquareBears.json';
 import MainSection from '../component/MainSection';
 
 const Minting = () => {
     const web3 = new Web3(window.ethereum);
-
-    const {MerkleTree} = require ('merkletreejs');
-    const keccak256 = require ('keccak256');
     
     const [userAddress, setUserAddres] = useState();
     const [walletConnected, setWalletConnected] = useState();
     const [scareBears, setScareBears] = useState();
-    const [balanceQty, setBalanceQty] = useState();
-    const [balanceId, setBalanceId] = useState();
     const [publicSalePrice, setPublicSalePrice] = useState();
     const [whitelistSalePrice, setWhitelistSalePrice] = useState();
+    const [whitelistSale, setWhitelistSale] = useState();
     const [loading, setLoading] = useState(false);
 
-    const scareBearsAddress = '0x275b2Cf8FD5542747486673A7D551A9b5617b0b3';
-    let whitelistAddresses = ["0x7Da8e9089A94b6D353E3AD13D76B8a765EA2038D", "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"];
-
-    
-  
-    const leafNode = whitelistAddresses.map(addr => keccak256(addr));
-    const merkleTree = new MerkleTree(leafNode, keccak256, {sortPairs: true});
+    const scareBearsAddress = '0xa2724362452c0E7E60FFC9D3621dEC95eA8EcF39';
 
     const connectWallet = async() =>{
         if(typeof window.ethereum != 'undefined'){
@@ -35,8 +25,6 @@ const Minting = () => {
             connectContract(tempAccount);
 
             setWalletConnected(true);
-
-            console.log('0x'+merkleRoot());
         }
     }
 
@@ -45,12 +33,6 @@ const Minting = () => {
             const tempScareBears = new web3.eth.Contract(ScareBearsABI.abi, scareBearsAddress);
             setScareBears(tempScareBears);
 
-            const tempBalanceQty = await tempScareBears.methods.balanceOf(tempAccount).call();
-            setBalanceQty(tempBalanceQty);
-
-            const tempBalanceId = await tempScareBears.methods.walletOf().call({from: tempAccount});
-            setBalanceId(tempBalanceId);
-
             const tempPublicSalePrice = await tempScareBears.methods.PUBLIC_SALE_PRICE().call();
             console.log(tempPublicSalePrice);
             setPublicSalePrice(tempPublicSalePrice);
@@ -58,44 +40,15 @@ const Minting = () => {
             const tempWhitelistSalePrice = await tempScareBears.methods.WHITELIST_SALE_PRICE().call();
             console.log(tempWhitelistSalePrice);
             setWhitelistSalePrice(tempWhitelistSalePrice);
+
+            const tempWhitelistSale = await tempScareBears.methods.whiteListSale.call();
+            setWhitelistSale(tempWhitelistSale);
+
         }
         catch(e){
 
         }
-    }
-
-    const merkleRoot = () =>{
-        return merkleTree.getRoot().toString('hex');
-    }
-
-    const hexProof = () =>{
-        let index = whitelistAddresses.indexOf(userAddress);
-        return merkleTree.getHexProof(leafNode[index]);
-    }
-
-    const publicMint = async() =>{
-        const qty = parseInt(document.getElementById('publicMintQty').value);
-        const price = String(parseInt(publicSalePrice) * qty);
-        try{
-            await scareBears.methods.mint(qty).send({value: price, from: userAddress});
-            setLoading(false);
-        }
-        catch(e){
-            console.log(e);
-        }
-    }
-
-    const whitelistMint = async() =>{
-        const qty = parseInt(document.getElementById('whitelistMintQty').value);
-        const price = String(parseInt(whitelistSalePrice) * qty);
-        console.log(hexProof());
-        try{
-            await scareBears.methods.whitelistMint(hexProof(), qty).send({value: price, from: userAddress});
-        }
-        catch(e){
-            console.log(e);
-        }
-    }
+    } 
 
     window.onload = () =>{
         if(typeof window.ethereum !== 'undefined'){
@@ -110,13 +63,14 @@ const Minting = () => {
     <>
         <MainSection
         userAddress={userAddress}
+        scareBears={scareBears}
         connectWallet={connectWallet}
-        balanceQty={balanceQty}
-        balanceId={balanceId}
         loading={loading}
-        whitelistMint={whitelistMint}
-        publicMint={publicMint}
         walletConnected={walletConnected}
+        publicSalePrice={publicSalePrice}
+        setLoading={setLoading}
+        whitelistSalePrice={whitelistSalePrice}
+        whitelistSale={whitelistSale}
         ></MainSection>
     </>
   )
